d673b2a1a12c9f1e46b630a8ae55f18bb1daa07a
Uwe Schmitt <uschmitt@mineway.de>
2012-09-10 17:28:16 +0200

    project layout done

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import os, sys
+
+version = '0.1'
+
+setup(name="autowrap",
+      version=version,
+      description='Generates Python Extension modules from Cython PXD files',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      entry_points = {
+            'console_scripts' : 
+            [
+                'autowrap = autowrap.Main:main',
+            ]
+      },
+      install_requires = ["cython",],
+      setup_requires = ["nose",],
+    
+     )
---
+++ tests/import_test.py
@@ -0,0 +1,5 @@
+
+def testImport():
+    import autowrap
+
+
