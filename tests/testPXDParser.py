5e589671e3a3cc5ee35f3ca4a9682136f9280736
Uwe Schmitt <uschmitt@mineway.de>
2012-12-09 17:19:39 +0000

    modified test

--- tests/testPXDParser.py
+++ tests/testPXDParser.py
@@ -11,7 +11,21 @@ def _parse(pxdFileName):
 
 
 def test_minimal():
-    cld, = _parse("minimal.pxd")
+    cld, = autowrap.PXDParser.parse_str_("""
+cdef extern from "Minimal.hpp":
+
+    cdef cppclass Minimal:
+        Minimal(int a)
+        int getA()
+        unsigned int method0(unsigned int input)
+        float method1(float input)
+        double method2(double input)
+        char method3(char input)
+
+        void overloaded(int inp)
+        void overloaded(float inp)
+    """)
+
     assert cld.name == "Minimal"
     assert cld.template_parameters  == None
 
