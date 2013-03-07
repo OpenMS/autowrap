70bef1532d7448ebcb2e2f11cfaa940f9ff6d992
Uwe Schmitt <uschmitt@mineway.de>
2013-03-07 14:37:48 +0100

    made autowrap cheesshop ready

--- setup.py
+++ setup.py
@@ -10,16 +10,31 @@ except ImportError:
 
 from setuptools import setup, find_packages
 
-version = '0.1'
+version = '0.2'
 
 setup(name="autowrap",
       version=version,
-      description='Generates Python Extension modules from Cython PXD files',
+      maintainer="Uwe Schmitt",
+      maintainer_email="uschmitt@mineway.de",
+      license="http://opensource.org/licenses/BSD-3-Clause",
+      platforms=["any"],
+      description='Generates Python Extension modules from commented Cython PXD files',
+      classifiers=["Development Status :: 3 - Alpha",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: BSD License",
+                   "Programming Language :: Python :: 2.7",
+                   "Topic :: Software Development :: Code Generators",
+                   ],
+      long_description="""
+autowrap automatically generates python extension modules for wrapping
+C++ libraries based on annotated (commented) cython pxd files. """,
+
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,   # see MANIFEST.in
       zip_safe=False,
       test_suite='nose.collector',
       tests_require=['nose'],
+      install_requires=['Cython>=0.18'],
       entry_points={
            'console_scripts':
            [
