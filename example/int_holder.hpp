509f3e217e6e59702ec6cbbd5666210911e83907
Hannes Roest <hroest@stanford.edu>
2017-04-13 11:10:50 -0700

    [DOC] fix examples

--- example/int_holder.hpp
+++ example/int_holder.hpp
@@ -1,9 +1,10 @@
 class IntHolder {
     public:
         int i_;
-        IntHolder(int i): i_(i) { };
-        IntHolder(const IntHolder & i): i_(i.i_) { };
-        int add(const IntHolder & other) {
+        IntHolder(int i): i_(i) {};
+        IntHolder(const IntHolder & i): i_(i.i_) {};
+        int add(const IntHolder & other) 
+        {
             return i_ + other.i_;
         }
 };
