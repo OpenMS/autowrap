644a4cc77525489839d6d3bbd4574e8119deafe4
Uwe <uwe.schmitt@id.ethz.ch>
2018-04-05 15:59:39 +0200

    bumpted to version 0.15.0

--- autowrap/version.py
+++ autowrap/version.py
@@ -30,7 +30,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-__version__ = (0, 14, 0)
+__version__ = (0, 15, 0)
 
 # for compatibility with older version:
 version = __version__
