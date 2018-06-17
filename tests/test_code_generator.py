f16d407a94730b6ca6ce5cd4e6fa8e4ffd646fc8
Hannes Roest <hroest@stanford.edu>
2018-06-17 11:45:17 -0400

    [TEST] fix

--- tests/test_code_generator.py
+++ tests/test_code_generator.py
@@ -164,8 +164,8 @@ def test_libcpp():
     assert libcpp.__name__ == "libcpp"
     print(dir(libcpp))
     assert len(libcpp.LibCppTest.__doc__) == 214
-    assert len(libcpp.LibCppTest.twist.__doc__) == 116
-    assert len(libcpp.LibCppTest.gett.__doc__) == 58
+    assert len(libcpp.LibCppTest.twist.__doc__) == 124
+    assert len(libcpp.LibCppTest.gett.__doc__) == 66
 
     sub_libcpp_copy_constructors(libcpp)
 
