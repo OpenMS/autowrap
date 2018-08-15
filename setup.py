dce5bfd70901b426674808f8bfcb6e96b77c8803
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2018-08-15 10:34:39 +0200

    bumped version to 0.19.0

--- setup.py
+++ setup.py
@@ -7,47 +7,40 @@ try:
 except ImportError:
     pass
 
-from setuptools import setup, find_packages
-
+from setuptools import find_packages, setup
 
 # DO NOT FORGET TO BUMP THE VERSION IN version.py !!!!!!!!!!!!!!!!!!!
-VERSION = (0, 18, 1)
+VERSION = (0, 19, 0)
 # DO NOT FORGET TO BUMP THE VERSION IN version.py !!!!!!!!!!!!!!!!!!!
 
 
-
-setup(name="autowrap",
-      version="%d.%d.%d" % VERSION,
-      maintainer="Uwe Schmitt",
-      maintainer_email="uschmitt@mineway.de",
-      license="http://opensource.org/licenses/BSD-3-Clause",
-      platforms=["any"],
-      description='Generates Python Extension modules from commented Cython PXD files',
-      classifiers=["Development Status :: 3 - Alpha",
-                   "Intended Audience :: Developers",
-                   "License :: OSI Approved :: BSD License",
-                   "Programming Language :: Python :: 2.6",
-                   "Programming Language :: Python :: 2.7",
-                   "Programming Language :: Python :: 3.3",
-                   "Programming Language :: Python :: 3.4",
-                   "Programming Language :: Python :: 3.5",
-                   "Programming Language :: Python :: 3.6",
-                   "Topic :: Software Development :: Code Generators",
-                   ],
-      long_description="""
+setup(
+    name="autowrap",
+    version="%d.%d.%d" % VERSION,
+    maintainer="Uwe Schmitt",
+    maintainer_email="uschmitt@mineway.de",
+    license="http://opensource.org/licenses/BSD-3-Clause",
+    platforms=["any"],
+    description="Generates Python Extension modules from commented Cython PXD files",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Code Generators",
+    ],
+    long_description="""
 autowrap automatically generates python extension modules for wrapping
 C++ libraries based on annotated (commented) cython pxd files. """,
-
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,   # see MANIFEST.in
-      zip_safe=False,
-      test_suite='nose.collector',
-      install_requires=['Cython>=0.19'],
-      entry_points={
-           'console_scripts':
-           [
-               'autowrap = autowrap.Main:main',
-           ]
-      },
-
-      )
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+    include_package_data=True,  # see MANIFEST.in
+    zip_safe=False,
+    test_suite="nose.collector",
+    install_requires=["Cython>=0.19"],
+    entry_points={"console_scripts": ["autowrap = autowrap.Main:main"]},
+)
