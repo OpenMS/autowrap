b14e0a17c684b0012b0a20c46e37b18379384299
Hannes Roest <hannes.rost@utoronto.ca>
2019-04-29 09:04:11 -0400

    [VERSION] bump version

--- autowrap/version.py
+++ autowrap/version.py
@@ -30,7 +30,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-__version__ = (0, 19, 0)
+__version__ = (0, 20, 0)
 
 # for compatibility with older version:
 version = __version__
