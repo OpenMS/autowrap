a5bf120a7e435954e1c00229d6effd6b44b957e8
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2021-02-25 13:28:16 +0100

    [VERSION] 0.22.2

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 from setuptools import find_packages, setup
 
 
-VERSION = (0, 22, 1)
+VERSION = (0, 22, 2)
 # DO NOT FORGET TO BUMP THE VERSION IN version.py !!!!!!!!!!!!!!!!!!!
 
 
