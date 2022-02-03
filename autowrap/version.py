836efdc1a97766fb6933cd14022ed2a12995c971
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2022-02-03 10:39:03 +0100

    Fix version for current dev

--- autowrap/version.py
+++ autowrap/version.py
@@ -30,8 +30,8 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-__version__ = (0, 22, 7)
+__version__ = (0, 22, 8)
 
 
 # for compatibility with older version:
-__version__ = (0, 22, 7)
+version = __version__
